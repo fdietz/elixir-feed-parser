ca586ba33f86270ae642acee9e197b7c77107840
Frederik Dietz <fdietz@gmail.com>
2016-04-18 12:52:02 +0200

    update version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ElixirFeedParser.Mixfile do
 
   def project do
     [app: :elixir_feed_parser,
-     version: "0.10.0",
+     version: "1.0.0",
      elixir: "~> 1.0",
      description: description,
      package: package,
